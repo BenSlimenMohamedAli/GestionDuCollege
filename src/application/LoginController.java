3ddb3427fdd1f4952b1b5b47bf9a45707abb0c8e
Dali15 <Dslimen.15@gmail.com>
2017-12-22 04:44:11 -0800

    premier pas

--- src/application/LoginController.java
+++ src/application/LoginController.java
@@ -1,24 +1,32 @@
 package application;
 
+import application.espaces.EspaceEnsController;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import main_package.*;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import application.espaces.*;
 
 
 public class LoginController {
     @FXML
-    Label result;
+    private AnchorPane ap;
     @FXML
-    private Button connecter;
+    private Label result;
     @FXML
     private TextField username;
     @FXML
     private PasswordField password;
-    @FXML
-    private ToggleGroup choix;
-    @FXML
-    private RadioButton choixEleve;
     @FXML
     private RadioButton choixEnseignant;
     @FXML
@@ -27,16 +35,45 @@ public class LoginController {
 
     @FXML
     protected void seConnecter(){
+        // Si le choix est enseignant
         if(choixEnseignant.isSelected()){
-            if( Enseignant.connecter(username.getText(),password.getText().equals("")? 0:Integer.parseInt(password.getText())) )
-                result.setText("Vous étes connecté"); // TODO
-            else
+            // Si l'enseignant existe
+            if( Enseignant.connecter(username.getText(),password.getText().equals("")? 0:Integer.parseInt(password.getText())) ){
+                try{
+                    Platform.runLater(()->{
+                        Stage ens = new Stage();
+                        Parent root = null;
+                        try {
+                            root = FXMLLoader.load(getClass().getResource("espaces/espace_ens.fxml"));
+                        } catch (IOException ex) {
+                            Logger.getLogger(EspaceEnsController.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                        Scene scene = new Scene(root);
+                        ens.setScene(scene);
+                        ens.show();
+                        ens.setMinHeight(600);
+                        ens.setMaxHeight(600);
+                        ens.setMinWidth(800);
+                        ens.setMaxWidth(800);
+                        ens.setTitle(String.valueOf(username.getText()));
+                        ap.getScene().getWindow().hide();
+                    });
+                }catch (Exception ex){
+                    ex.printStackTrace();
+                }
+            }
+            // Si l'enseignant n'existe pas
+            else{
                 result.setText("Vérifier vos données");
+            }
+        // Si le choix est eleve
         }else{
+            // Si léleve est existe
             if(Eleve.connecter(username.getText(),password.getText().equals("")? 0:Integer.parseInt(password.getText()))){
                 result.setText("Vous étes connecté");
+            // S'il n'existe pas
             }else{
-                result.setText("Verifier vos données");
+                result.setText("Vérifier vos données");
             }
         }
     }
---
+++ src/application/espaces/EspaceEnsController.java
@@ -0,0 +1,16 @@
+package application.espaces;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import sun.plugin.javascript.navig.Anchor;
+
+
+public class EspaceEnsController {
+    @FXML
+    private AnchorPane ap;
+
+    public void initialize() {
+
+    }
+}
